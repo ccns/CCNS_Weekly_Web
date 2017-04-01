94d900278449e33f3f0afdc690c1c26d8687998f
Daniel Chen <team6612@gmail.com>
2017-04-01 18:46:16 +0800

    connect to mc api

--- scripts/publisher.js
+++ scripts/publisher.js
@@ -1,5 +1,7 @@
 var juice = require('juice');
 var fs = require('fs');
+var Mailchimp = require('mailchimp-api-v3')
+
 hexo.extend.generator.register('mail', function(locals){
   var config = this.config;
   var posts = locals.posts.sort(config.index_generator.order_by);
@@ -10,8 +12,32 @@ hexo.extend.generator.register('mail', function(locals){
   }
 });
 hexo.extend.deployer.register('mailchimp', function(args){
-  console.log(args.api_key);
+  var mailchimp = new Mailchimp(args.api_key);
+  var list_id = args.list_id;
+  var from_name = args.from_name;
+  var reply_to = args.reply_to;
   juice.juiceFile('public/mail.html', {}, function(err, html){
     fs.writeFileSync('mail_c.html', html);
+    var title = /<title>(.*)<\/title>/.exec(html)[1];
+    var campaign_id;
+    mailchimp.post('/campaigns', {
+      "recipients": {
+        "list_id": list_id
+      },
+      "type":"regular",
+      "settings": {
+        "subject_line": title,
+        "reply_to": reply_to,
+        "from_name": from_name
+      }
+    }).then(function(results) {
+      campaign_id = results.id;
+      return mailchimp.put('/campaigns/'+campaign_id+'/content', { "html": html })
+    }).then(function(results) {
+      return mailchimp.post('/campaigns/'+campaign_id+'/actions/send')
+    }).catch(function(err) {
+      console.log(err);
+      throw new Error(err);
+    })
   })
 });
